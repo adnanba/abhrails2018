52b90ba481c801d70e5dfcdc566a52b339d4467b
adnanba <ababovic1@etf.unsa.ba>
2018-01-29 14:50:57 +0100

    created login and signup actions for app

--- Webshop/app/models/user.rb
+++ Webshop/app/models/user.rb
@@ -2,9 +2,11 @@ class User < ApplicationRecord
     belongs_to :role
     has_many :purchases, dependent: :destroy
 
+    has_secure_password
+
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 255 },
                       format: { with: VALID_EMAIL_REGEX }
     
-    validates :password, presence: true, length: { minimum: 8, maximum: 16 }
+    #validates :password, presence: true, length: { minimum: 8, maximum: 16 }
 end
