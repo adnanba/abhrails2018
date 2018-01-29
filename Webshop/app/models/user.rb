60bcdb196f9cab91834176cab8cdb6e1b101c6cf
adnanba <ababovic1@etf.unsa.ba>
2018-01-29 09:46:42 +0100

    Added roles to database, added validation to model User

--- Webshop/app/models/user.rb
+++ Webshop/app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
     belongs_to :role
     has_many :purchases, dependent: :destroy
+
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, presence: true, length: { maximum: 255 },
+                      format: { with: VALID_EMAIL_REGEX }
+    
+    validates :password, presence: true, length: { minimum: 8, maximum: 16 }
 end
